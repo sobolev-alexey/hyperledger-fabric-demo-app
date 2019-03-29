cfdcf99094b644ccbbc671cb84c8e613015c9384
Alexey Sobolev <sobolev@mail.de>
2019-03-29 17:37:19 +0100

    Chaincode path update

--- startFabric.sh
+++ startFabric.sh
@@ -25,7 +25,7 @@ cd ./basic-network
 # and prime the ledger with our 10 containers
 docker-compose -f ./docker-compose.yml up -d cli
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n hyperledger-fabric-demo-app -v 1.0 -p github.com/sobolev-alexey/hyperledger-fabric-demo-app/chaincode/hyperledger-fabric-demo-app
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n hyperledger-fabric-demo-app -v 1.0 -p github.com/sobolev-alexey/hyperledger-fabric-demo-app/chaincode
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n hyperledger-fabric-demo-app -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n hyperledger-fabric-demo-app -c '{"function":"initLedger","Args":[""]}'
